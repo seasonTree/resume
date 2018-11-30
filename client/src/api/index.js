d1967708a7846e0b25c42852cb0ef98ece1016a0
Kuang917 <775803639@qq.com>
2018-11-30 18:49:25 +0800

    kh

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -16,4 +16,4 @@ const install = function (Vue) {
     })
 };
 
-export default install;
+                 export default install;
--- server/runtime/temp/bc5c09588a4ff10ada82b8fe941b481c.php
+++ server/runtime/temp/bc5c09588a4ff10ada82b8fe941b481c.php
@@ -1,4 +1,4 @@
-<?php /*a:1:{s:32:"H:\resume/client/dist/index.html";i:1543570823;}*/ ?>
+<?php /*a:1:{s:32:"H:\resume/client/dist/index.html";i:1543571013;}*/ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,8 +8,8 @@
     <link rel="icon" href="/favicon.ico" type="img/x-ico" />
     <title>简历管理系统</title>
     
-<link href="/css/runtime~appdaa9.css" rel="stylesheet"><link href="/css/appdaa9.css" rel="stylesheet"></head>
+<link href="/css/runtime~app6760.css" rel="stylesheet"><link href="/css/app6760.css" rel="stylesheet"></head>
 <body>
     <div id="app"></div>
-<script type="text/javascript" src="/runtime~app/indexdaa9.js"></script><script type="text/javascript" src="/js/chunks/appdaa9.js"></script></body>
+<script type="text/javascript" src="/runtime~app/index6760.js"></script><script type="text/javascript" src="/js/chunks/app6760.js"></script></body>
 </html>
