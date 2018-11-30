0412bf4a4c8270753112f2bc073d3dd7e2fc0e5f
seasonTree <leung_chikin@outlook.com>
2018-11-30 11:21:24 +0800

    修改数据库

--- db.sql
+++ db.sql
@@ -8,16 +8,16 @@ use resume;
 drop table if exists user;
 create table user
 (
-	id int auto_increment primary key,
-	uname varchar(64) not null,
-	passwd varchar(128) not null,
-    pesonal_name varchar(32) not null default '', /* 姓名 */
-    status tinyint(1) not null default 0, /* 0: 正常， 1: 禁用 */
-	ct_user varchar(64) default '' null,
-	ct_time datetime default CURRENT_TIMESTAMP not null ,
-	mfy_user varchar(64) default '' null,
-	mfy_time datetime default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP not null,
+	id int auto_increment primary key comment '自增id',
+	uname varchar(64) not null comment '用户名',
+	passwd varchar(256) not null comment '密码',
+    pesonal_name varchar(32) not null default '' comment '姓名',
+    status tinyint(1) not null default 0 comment '0: 正常， 1: 禁用',
+	ct_user varchar(64) default '' null comment '创建人',
+	ct_time datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+	mfy_user varchar(64) default '' null comment '修改人',
+	mfy_time datetime default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP not null comment '修改时间',
 	unique index `user_uname` (`uname`)
 ) engine=InnoDB;
 
-insert into user(uname, passwd) values('admin', '7c4a8d09ca3762af61e59520943dc26494f8941b');
+insert into user(uname, passwd) values('admin', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
