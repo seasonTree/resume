6df24ca48752a15be3a24c6b4393f7a58ca0bd95
祈 <yuqi2060@163.com>
2018-12-26 18:14:23 +0800

    sphinx

--- server/application/index/controller/Resume.php
+++ server/application/index/controller/Resume.php
@@ -707,7 +707,7 @@ class Resume extends Controller
             $n++;
         }
 
-        $data1 = $sphinx->query('@name d & @sex 男 & @educational 本科 & @phone 123 & @email 21@12',"resume");   //星号为所有索引源
+        $data1 = $sphinx->query('@name d & @sex 男 & @educational 本科 & @phone 123',"resume");   //星号为所有索引源
         $data = array_column($result['matches'],'id');
         echo $phinx_where;
         dump($data1);exit;
