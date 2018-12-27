11edaaca61995a4d00a5ebbeb422c2807020ea1b
祈 <yuqi2060@163.com>
2018-12-27 15:21:17 +0800

    merge

--- server/application/index/controller/Resume.php
+++ server/application/index/controller/Resume.php
@@ -807,7 +807,7 @@ class Resume extends Controller
         }
         if (!empty($ids)) {
             $sphinx->SetFilter('id',$ids);
-            $data = $sphinx->query('','resume');
+            $data = $sphinx->SetSortMode( SPH_SORT_ATTR_DESC, "resume" );
         }
         
         dump($data);
