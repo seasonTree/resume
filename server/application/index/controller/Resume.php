5b999aa3d772c7f355ebd730336a19683d1e9cb3
祈 <yuqi2060@163.com>
2018-12-28 15:04:52 +0800

    checkname

--- server/application/index/controller/Resume.php
+++ server/application/index/controller/Resume.php
@@ -914,7 +914,7 @@ class Resume extends Controller
             $data_arr = $res['matches'];
 
             foreach ($data_arr as $k => $v) {
-                array_unshift($v['attrs'],$v['id'])
+                array_unshift($v['attrs'],$v['id']);
                 $data[$k] = $v['attrs'];
             }
 
