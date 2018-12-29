c8a0466728d3e281934aa82b4928394d8f57f6f6
祈 <yuqi2060@163.com>
2018-12-29 11:27:14 +0800

    auth

--- server/application/index/controller/Resume.php
+++ server/application/index/controller/Resume.php
@@ -928,8 +928,8 @@ class Resume extends Controller
             $data_num = 0;//定义数据量
             $page_end = $where['pageSize'] * $where['pageIndex'];
             $page_start = $page_end - $where['pageSize'];
-            $data_arr = array_slice($data_arr,$page_start,$where['pageSize']);
-            foreach ($data_arr as $k => $v) {
+            $list_arr = array_slice($data_arr,$page_start,$where['pageSize']);
+            foreach ($list_arr as $k => $v) {
                 // if ($k < $page_start) {
                 //     continue;//分页处理，过滤不符合要求的数据
                 // }
