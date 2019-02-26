12b1546605311064d7ba78bf06e5c0450c75a02f
yuqi2060 <yuqi2060@163.com>
2019-02-26 12:00:22 +0800

    ct_user

--- server/application/index/controller/Resume.php
+++ server/application/index/controller/Resume.php
@@ -1474,10 +1474,10 @@ class Resume extends Controller
             $ct_user_length = count($ct_user)-1;
             foreach ($ct_user as $k => $v) {
                 if ($k == $ct_user_length) {
-                    $ct_user_where.=$ct_user_where."@ct_user ($v) )";
+                    $ct_user_where.=$ct_user_where."@ct_user $v )";
                 }
                 else{
-                    $ct_user_where.=$ct_user_where."@ct_user ($v) |";
+                    $ct_user_where.=$ct_user_where."@ct_user $v |";
                 }
                 
             }
