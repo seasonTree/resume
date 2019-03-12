99d3d81958f72c8b0946f9e37be2600901b0d326
yuqi2060 <yuqi2060@163.com>
2019-03-12 17:57:53 +0800

    ct_time

--- server/application/index/controller/Resume.php
+++ server/application/index/controller/Resume.php
@@ -1298,7 +1298,7 @@ class Resume extends Controller
     }
     public function test(){
         // dump(file_get_contents(dirname(Env::get('ROOT_PATH')).'/server/extend/speciality.txt'));
-        file_put_contents(dirname(Env::get('ROOT_PATH')).'/server/extend/speciality.txt','|sdf',FILE_APPEND);
+        file_put_contents(dirname(Env::get('ROOT_PATH')).'/server/extend/test.txt','|sdf',FILE_APPEND);
         exit;
         // $source = dirname(Env::get('ROOT_PATH')).'/client/dist/uploads/file/test.docx';
         // $text = new \Docx2Text();
