4b839379ec2101d17d05042cadbde6cf15c0e6a4
yuqi2060 <yuqi2060@163.com>
2019-04-03 10:29:43 +0800

    comm

--- server/application/index/controller/Communicate.php
+++ server/application/index/controller/Communicate.php
@@ -26,6 +26,7 @@ class Communicate
          $cli_res = $cli_model->field('id,client_name')->select()->toArray();
          $comm_data = [];//沟通数据
          $comm_user = [];//沟通人
+         $del_ids = [];//处理多余没用的数据的id，mysql里面删掉(针对批量导入多次重复的沟通记录)
          foreach ($cli_res as $k => $v) {
            $cli_name[$v['id']] = $v['client_name'];
          }
@@ -33,37 +34,34 @@ class Communicate
          foreach ($data as $k => $v) {
 
             if (in_array($v['content'], $comm_data)) {
+              //判断该沟通记录是否已经存在
                 $comm_key = array_search($v['content'],$comm_data);
+                //把存在对应的key取出来
                 // dump($comm_key);
                 if($comm_user[$comm_key] == $v['ct_user']){
-
+                  //再查看对应的沟通记录的沟通人和这一条是否一致
                   if (!$data[$k]['screen'] && !$data[$k]['arrange_interview'] && !$data[$k]['arrive'] && !$data[$k]['approved_interview'] && !$data[$k]['entry']) {
                         //判断重复数据里面是否有全零，如果有删除并跳过
+                        $del_ids[] = $data[$k]['id'];//记录应该要删除记录的id
                         unset($data[$k]);
                         continue;
                    }
-                   
+
                    if (!$data[$comm_key]['screen'] && !$data[$comm_key]['arrange_interview'] && !$data[$comm_key]['arrive'] && !$data[$comm_key]['approved_interview'] && !$data[$comm_key]['entry']) {
                         //判断全部为0的时候，表示是重复数据，不输出并删除
+                        $del_ids[] = $data[$comm_key]['id'];//记录应该删除的记录id
                         unset($data[$comm_key]);
                         continue;
                    }
-
-                   // else{
-                   //      unset($data[$comm_key]);
-                   //      unset($comm_user[$comm_key]);
-                   //      unset($comm_data[$comm_key]);
-                   // }
-                   
                 }
             }
-            $comm_user[$k] = $v['ct_user'];
+            $comm_user[$k] = $v['ct_user'];//存储每一次的沟通人记录
             $comm_data[$k] = $v['content'];
 
             $cli_comm_list = $cli_comm_model->where(['comm_id' => $v['id']])->field('type,client_id')->select()->toArray();
 
             foreach ($cli_comm_list as $a => $b) {
-
+              //把沟通列表进行处理，加入客户名
                switch ($b['type']) {
                  case 1:
                    $data[$k]['screen_name'] = isset($data[$k]['screen_name'])?$data[$k]['screen_name'].','.$cli_name[$b['client_id']]:$cli_name[$b['client_id']];
@@ -87,6 +85,7 @@ class Communicate
                }
 
             }
+            //如果不存在客户名，直接返回空
             !isset($data[$k]['screen_name'])?$data[$k]['screen_name']='':'';
             !isset($data[$k]['arrange_interview_name'])?$data[$k]['arrange_interview_name']='':'';
             !isset($data[$k]['arrive_name'])?$data[$k]['arrive_name']='':'';
@@ -94,7 +93,12 @@ class Communicate
             !isset($data[$k]['entry_name'])?$data[$k]['entry_name']='':'';
 
          }
-
+         if (!empty($del_ids)) {
+          //删除多余的数据
+            $del_ids = implode(',',$del_ids);
+            $comm->where("id in($del_ids)")->delete();
+         }
+         
          return json(['msg' => '获取成功','code' => 0,'data' => $data]);
        }
        else{
