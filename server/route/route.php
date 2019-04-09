7559eac0af5e5141170bbe73bb5003ccf8d0126e
seasonTree <leung_chikin@outlook.com>
2019-04-09 15:22:17 +0800

    页面

--- server/route/route.php
+++ server/route/route.php
@@ -108,8 +108,8 @@ Route::get('api/resume/export','Resume/export');
 
 //客户相关
 /***新加功能***/
+Route::get('api/report/client_statistics/export','Report/clientRecExport');
 Route::get('api/report/client_statistics','Report/clientRec');
-// Route::get('api/report/client_statistics/export','Report/clientRecExport');
 /******************************************************************/
 Route::get('api/client/list','Client/getList');//客户列表
 Route::post('api/client/add','Client/addClient');//添加客户
